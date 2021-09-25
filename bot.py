e03ee42284932f5ee3066f20547a50e6be82ff18
嚕梯 <59413200+asddzxcc1856@users.noreply.github.com>
2021-09-25 23:25:11 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
-print("Hello World")
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!!')
+
+@bot.event
+async def on_ready():
+    print(">> Bot is Online <<")
+
+@bot.event
+async def on_member_join(member):
+    print(f'{member} join!')
+
+@bot.event
+async def on_member_remove(member):
+    print(f'{member} leave!')
+
+bot.run('ODkxMzIzMTgzMjk3MzMxMjAw.YU8rZg.JX5_sCQX91uvJsVPwBT1_DJ7TAg')
+
