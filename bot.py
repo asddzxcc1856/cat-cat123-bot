7ef373df8302ad134db4e2cb79edc161584333ee
嚕梯 <59413200+asddzxcc1856@users.noreply.github.com>
2021-09-26 02:05:19 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,12 +10,12 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     channel = bot.get_channel(846916301364527177)
-    await channel.send(f'{member} join!')
+    await channel.send(f"{member} join!")
 
 @bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(846916301364527177)
-    await channel.send(f'{member} leave!')
+    channel = bot.get_channel(846917170947424256)
+    await channel.send(f"{member} leave!")
 
 bot.run('ODkxMzIzMTgzMjk3MzMxMjAw.YU8rZg.JX5_sCQX91uvJsVPwBT1_DJ7TAg')
 
