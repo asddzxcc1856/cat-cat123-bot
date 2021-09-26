2f2d7434b25de6297c7c369f6853b2c9784c6f87
嚕梯 <59413200+asddzxcc1856@users.noreply.github.com>
2021-09-26 13:49:57 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import discord
+intents = discord.Intents.default()
+intents.members = True
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!!')
+bot = commands.Bot(command_prefix='!!', intents = intents)
 
 @bot.event
 async def on_ready():
